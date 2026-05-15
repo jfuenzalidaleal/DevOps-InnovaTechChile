972fdfefb39e98324b9b71a6e25012c255ad894b
Elias Casa <eliasaaronalfaro@gmail.com>
2026-05-15 18:12:18 -0400

    fix: direcciones ips

--- front_despacho/src/componentes/CrudAdmin/TableCompras.jsx
+++ front_despacho/src/componentes/CrudAdmin/TableCompras.jsx
@@ -7,7 +7,7 @@ export const TableCompras = () => {
   const [ventas, setVentas] = useState([]);
 
   const compras = async () => {
-    await axios.get("http://192.168.30/api/v1/ventas", {
+    await axios.get("http://10.0.141.91/api/v1/ventas", {
       headers:{
         'Content-Type': 'application/json',
         'Accept': 'application/json'
